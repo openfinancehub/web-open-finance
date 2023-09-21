e1b9eb8946d644e5c44a4a9fb3a6385ed4069eba
Chaya <>
2023-09-21 11:56:07 +0800

    fix: 修改域名

--- src/services/LoginServices/index.ts
+++ src/services/LoginServices/index.ts
@@ -6,13 +6,18 @@ import { REQUEST } from '../AxionsInstance';
 
 export class LoginServices {
   public static getCaptcha(params: any): Promise<any> {
-    return REQUEST({ url: ApiUrl.GET_CAPTCHA, params });
+    return REQUEST({
+      url: ApiUrl.GET_CAPTCHA,
+      params,
+      baseURL: 'http://39.101.71.109/app-api'
+    });
   }
   public static login(data: any): Promise<any> {
     return REQUEST({
       method: 'POST',
-      url: '/login',
-      data
+      url: ApiUrl.LOGIN,
+      data,
+      baseURL: 'http://39.101.71.109/app-api'
     });
   }
 }
