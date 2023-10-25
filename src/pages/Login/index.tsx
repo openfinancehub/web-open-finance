0e8c051b8852404eb045e57a526ac08e055d0f13
Wang Yin Peng <982172159@qq.com>
2023-10-25 19:10:07 +0800

    修改models图标界面

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -32,13 +32,14 @@ export default () => {
     try {
       const { mobile: username, captcha } = values;
       const res = await LoginServices.login({ username, captcha });
+      history.push('/home');
       if (res?.code === 0) {
-        await setInitialState(s => ({
-          ...s,
-          currentUser: res.data
-        }));
-        localStorage.setItem('userInfo', JSON.stringify(res.data));
-        history.push('/home');
+        // await setInitialState(s => ({
+        //   ...s,
+        //   currentUser: res.data
+        // }));
+        // localStorage.setItem('userInfo', JSON.stringify(res.data));
+        // history.push('/home');
       } else {
         message.error('服务器错误');
       }
