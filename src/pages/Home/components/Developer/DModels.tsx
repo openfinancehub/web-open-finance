2e344dd100374a8b8ff7e9bc3b708d8935e430da
Wang Yin Peng <982172159@qq.com>
2024-01-13 18:28:25 +0800

    修改标题和list数据

--- src/pages/Home/components/Developer/DModels.tsx
+++ src/pages/Home/components/Developer/DModels.tsx
@@ -17,11 +17,22 @@ interface ModelsProps {
 }
 
 const DModels: React.FC<ModelsProps> = ({ onFilterFinance, data, company, setCompany, isDeveloper }) => {
-    const [filteredModels, setFilteredModels] = useState(data);
+    // const [filteredModels, setFilteredModels] = useState(data);
+    const [filteredModelsMap, setFilteredModelsMap] = useState(new Map());
     const [modelValue, setModelValue] = useState('');
     const [factorValue, setFactorValue] = useState('');
     const [showModal, setShowModal] = useState(false);
     const [categoryList, setCategoryList] = useState<ListCategoryType>([]);
+
+
+    // 添加元素到Map数组
+    const addElement = (key: string, value: ModelsItem[]) => {
+        setFilteredModelsMap(prevMapArray => {
+            const newMapArray = new Map(prevMapArray);
+            newMapArray.set(key, value);
+            return newMapArray;
+        });
+    };
 
     const fetchCategoryJson = async () => {
         try {
@@ -48,8 +59,10 @@ const DModels: React.FC<ModelsProps> = ({ onFilterFinance, data, company, setCom
     const handleTriggerEvent = async (factor: string) => {
         const dataJson = await getModels(factor);
         if (dataJson?.result?.models != null && dataJson.result.ret_code == 0) {
-            setFilteredModels(dataJson?.result?.models)
+            // setFilteredModels(dataJson?.result?.models)
+            addElement(factor, dataJson?.result?.models)
         }
+
     };
     //跳转models详情
     const onRow = (record: any) => {
@@ -65,11 +78,11 @@ const DModels: React.FC<ModelsProps> = ({ onFilterFinance, data, company, setCom
     const showModalChange = () => {
         setShowModal((prevShowModal) => !prevShowModal);
     };
-    const fetchData = async () => {
-        setFilteredModels(data);
-    };
+    // const fetchData = async () => {
+    //     setFilteredModels(data);
+    // };
     useEffect(() => {
-        fetchData();
+        // fetchData();
         fetchCategoryJson();
     }, [data]);
 
@@ -92,11 +105,38 @@ const DModels: React.FC<ModelsProps> = ({ onFilterFinance, data, company, setCom
         );
     };
 
+    const FactorMeta: React.FC<{ factor: string }> = ({ factor }) => {
+        return (
+            <ProList<ModelsItem>
+                onRow={onRow}
+                rowKey='id'
+                dataSource={filteredModelsMap.get(factor)}
+                showActions='hover'
+                metas={{
+                    title: {
+                        dataIndex: 'model',
+                        title: '模型名称',
+                    },
+                    avatar: {
+                        dataIndex: 'icon',
+                        search: false,
+                    },
+                    description: {
+                        dataIndex: 'tag',
+                        search: false,
+                        render: (_, json) => <DescriptionMeta json={json} />,
+                    },
+                }}
+            />
+        );
+    };
+
     const onChange = (key: string | string[]) => {
         console.log(key);
         if (key) {
             handleTriggerEvent(key as string);
         }
+        // console.log(filteredModelsList)
     };
 
     return (
@@ -107,29 +147,9 @@ const DModels: React.FC<ModelsProps> = ({ onFilterFinance, data, company, setCom
                         <Panel header={item.title} key={item.title}>
                             {/* {item.title} */}
                             {item.description.map(({ factor }, index) => (
-                                <Collapse bordered={false} accordion >
-                                    <Panel header={item.title + factor} key={item.title + factor} >
-                                        <ProList<ModelsItem>
-                                            onRow={onRow}
-                                            rowKey='id'
-                                            dataSource={filteredModels}
-                                            showActions='hover'
-                                            metas={{
-                                                title: {
-                                                    dataIndex: 'model',
-                                                    title: '模型名称',
-                                                },
-                                                avatar: {
-                                                    dataIndex: 'icon',
-                                                    search: false,
-                                                },
-                                                description: {
-                                                    dataIndex: 'tag',
-                                                    search: false,
-                                                    render: (_, json) => <DescriptionMeta json={json} />,
-                                                },
-                                            }}
-                                        />
+                                <Collapse bordered={false} key={item.title + factor} accordion onChange={onChange}>
+                                    <Panel header={factor} key={factor} >
+                                        <FactorMeta factor={factor} />
                                     </Panel>
                                 </Collapse>
                             ))}
@@ -149,7 +169,7 @@ const DModels: React.FC<ModelsProps> = ({ onFilterFinance, data, company, setCom
                     company={company}
                     setCompany={setCompany} />
             </div>}
-        </div>
+        </div >
     );
 };
 
