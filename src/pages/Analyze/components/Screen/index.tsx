70147cbcca61c3752cac0d3a9aa869d51593f4b5
shuhangya <2692958739@qq.com>
2024-05-09 22:01:11 +0800

    111

--- src/pages/Analyze/components/Screen/index.tsx
+++ src/pages/Analyze/components/Screen/index.tsx
@@ -1,8 +1,37 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState,useRef } from 'react';
 import { request } from 'umi';
+import { ProCard } from '@ant-design/pro-components';
 import { Select, Button, Input, Space,message  } from 'antd';
 import { useModel } from '@umijs/max';
-
+import * as echarts from 'echarts/core';
+import {
+    ToolboxComponent,
+    TooltipComponent,
+    GridComponent,
+    VisualMapComponent,
+    LegendComponent,
+    BrushComponent,
+    DataZoomComponent
+} from 'echarts/components';
+import {
+    CandlestickChart, LineChart, BarChart
+} from 'echarts/charts';
+import { UniversalTransition } from 'echarts/features';
+import { CanvasRenderer } from 'echarts/renderers';
+echarts.use([
+    ToolboxComponent,
+    TooltipComponent,
+    GridComponent,
+    VisualMapComponent,
+    LegendComponent,
+    BrushComponent,
+    DataZoomComponent,
+    CandlestickChart,
+    LineChart,
+    BarChart,
+    CanvasRenderer,
+    UniversalTransition
+]);
 const Screen = () => {
     const {
         initialState: { currentUser }
@@ -10,8 +39,11 @@ const Screen = () => {
     const [factor, setFactor] = useState([])
     const [mode, setMode] = useState([])
     const [facotrLi, setFactorLi] = useState('')
+    let   chart: any
     const [modeLi, setModeLi] = useState('')
     const [inputValue,setInputValue] = useState('')
+    const [echartsData,setEchartsData] = useState([])
+    const chartRef = useRef(null);
     const commonHeader = {
         user: currentUser.username,
         req_id: currentUser.id,
@@ -47,8 +79,10 @@ const Screen = () => {
     const postFetch = () => {
         const data = {
             factor: facotrLi,
-            mode: modeLi,
-            val: inputValue,
+            // mode: modeLi,
+            mode: "gt",
+            val: 0.01,
+            // val: inputValue,
             extra:''
         }
         request('quent-api/fetch', {
@@ -56,15 +90,66 @@ const Screen = () => {
             headers: {
                 'Content-Type': 'application/json',
             },
-            data: data,
-            header:commonHeader
+            // data:data,
+            data: {
+                data:[data],
+                // header:commonHeader
+            },
+           
         }).then((res)=>{
             message.info(res.msg)
+            const resData = res.result.DividentMean
+            const xData =  Object.keys(resData.result)
+            const yData = Object.values(resData.result)
+            setEchartsData([xData,yData])
         })
     }
     useEffect(() => {
         getFactor()
     }, [])
+    useEffect(()=>{
+        const option = {
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {
+                  type: 'shadow'
+                }
+              },
+              grid: {
+                left: '3%',
+                right: '4%',
+                bottom: '3%',
+                containLabel: true
+              },
+              xAxis: [
+                {
+                  type: 'category',
+                  data: echartsData[0],
+                  axisTick: {
+                    alignWithLabel: true
+                  }
+                }
+              ],
+              yAxis: [
+                {
+                  type: 'value'
+                }
+              ],
+              series: [
+                {
+                  name: '2024-4-27',
+                  type: 'bar',
+                  barWidth: '60%',
+                  data: echartsData[1]
+                }
+              ]
+        }
+        chart = echarts.init(chartRef.current);
+        chart.setOption(option);
+        return () => {
+            chart.dispose();
+        };
+    },[echartsData])
     function handleFacChange(value: any) {
         console.log(value);
         setFactorLi(value)
@@ -100,6 +185,9 @@ const Screen = () => {
                 />
                 <Button type="primary" onClick={handleQuery}>查询</Button>
             </Space>
+            <ProCard style={{ height: '40vh', marginTop:'30px' }} bordered>
+                <div ref={chartRef} style={{ width: "100%", height: "100%", overflow: 'hidden' }}></div>
+            </ProCard>
         </div>
     )
 }
