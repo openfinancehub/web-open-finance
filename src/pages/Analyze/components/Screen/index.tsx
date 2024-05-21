66895d39c61d12855275bd7b568a57fe0f810b89
舒航ya <2691958739@qq.com>
2024-05-21 11:56:53 +0800

    更新筛选列表

--- src/pages/Analyze/components/Screen/index.tsx
+++ src/pages/Analyze/components/Screen/index.tsx
@@ -1,236 +1,204 @@
-import { useEffect, useState,useRef } from 'react';
-import { request } from 'umi';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 import { ProCard } from '@ant-design/pro-components';
-import { Select, Button, Space,message,InputNumber,Table  } from 'antd';
 import { useModel } from '@umijs/max';
-import * as echarts from 'echarts/core';
-import {
-    ToolboxComponent,
-    TooltipComponent,
-    GridComponent,
-    VisualMapComponent,
-    LegendComponent,
-    BrushComponent,
-    DataZoomComponent
-} from 'echarts/components';
-import {
-    CandlestickChart, LineChart, BarChart
-} from 'echarts/charts';
-import { UniversalTransition } from 'echarts/features';
-import { CanvasRenderer } from 'echarts/renderers';
-echarts.use([
-    ToolboxComponent,
-    TooltipComponent,
-    GridComponent,
-    VisualMapComponent,
-    LegendComponent,
-    BrushComponent,
-    DataZoomComponent,
-    CandlestickChart,
-    LineChart,
-    BarChart,
-    CanvasRenderer,
-    UniversalTransition
-]);
+import { Button, Form, InputNumber, Select, Space, Table, message } from 'antd';
+import { useEffect, useState } from 'react';
+import { request } from 'umi';
+
 const Screen = () => {
-    const {
-        initialState: { currentUser }
-      } = useModel('@@initialState');
-    const [factor, setFactor] = useState([])
-    const [mode, setMode] = useState([])
-    const [facotrLi, setFactorLi] = useState('')
-    let   chart: any
-    const [modeLi, setModeLi] = useState('')
-    const [inputValue,setInputValue] = useState(0.01)
-    const [echartsData,setEchartsData] = useState([])
-    const [dataSource,setDataSource] = useState([])
-    const [columns,setColumns] = useState([])
-    const [condition,setCondition] = useState([1])
-    const chartRef = useRef(null);
-    const commonHeader = {
-        user: currentUser.username,
-        req_id: currentUser.id,
-        req_src: currentUser.avatarUrl,
-        token: currentUser.token
-    }
-    const getFactor = () => {
-        request('/quent-api/factor', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        }).then((res) => {
-            setFactorLi(res.result.factor[0])
-            setModeLi(res.result.mode[0])
-            let factorData = res.result.factor.map((item: string) => {
-                return {
-                    value: item,
-                    lable: item
-                }
-            });
-            let modeData = res.result.mode.map((item: string) => {
-                return {
-                    value: item,
-                    lable: item,
-                }
-            })
-            setFactor(factorData)
-            setMode(modeData)
-        })
-    }
-    
-    const postFetch = () => {
-        const data = {
-            factor: facotrLi,
-            mode: modeLi,
-            val: inputValue,
-            // extra:''
-        }
-        request('quent-api/fetch', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            data: {
-                data:[data],
-            },
-           
-        }).then((res)=>{
-            message.info(res.msg)
-            const resData = res.result
-            const arr:any = []
-            const title =  Object.keys(resData)
-            const firstTitle = title[0]            
-            const row = Object.keys(resData[firstTitle].TIME)
-            const columnsone =  title.map((item)=>{
-                arr.push(Object.values(resData[item].result))
-                return {
-                  title:item,
-                  dataIndex: item,
-                  key: item,
-                }
-            })
-            const firstName = {
-                title:'Name',
-                dataIndex:'name',
-                key:'name'
-            }
+  const {
+    initialState: { currentUser }
+  } = useModel('@@initialState');
+  const [factor, setFactor] = useState([]);
+  const [mode, setMode] = useState([]);
+  const [facotrLi, setFactorLi] = useState('');
+  const [modeLi, setModeLi] = useState('');
+  const [inputValue, setInputValue] = useState(0.01);
+  const [dataSource, setDataSource] = useState([]);
+  const [columns, setColumns] = useState([]);
+  // const commonHeader = {
+  //     user: currentUser.username,
+  //     req_id: currentUser.id,
+  //     req_src: currentUser.avatarUrl,
+  //     token: currentUser.token
+  // }
+  const getFactor = () => {
+    request('/quent-api/factor', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      setFactorLi(res.result.factor[0]);
+      setModeLi(res.result.mode[0]);
+      let factorData = res.result.factor.map((item: string) => {
+        return {
+          value: item,
+          lable: item
+        };
+      });
+      let modeData = res.result.mode.map((item: string) => {
+        return {
+          value: item,
+          lable: item
+        };
+      });
 
-            const tableData = row.map((name,i)=>{
-                const rowData = { name };
-                title.forEach((column, j) => {
-                    rowData[column] = arr[j][i];
-                  });
-                return rowData;
-            })
-            setColumns([firstName,...columnsone])
-            setDataSource(tableData)
-        })
-    }
-    useEffect(() => {
-        getFactor()
-    }, [])
-    // useEffect(()=>{
-    //     const option = {
-    //         tooltip: {
-    //             trigger: 'axis',
-    //             axisPointer: {
-    //               type: 'shadow'
-    //             }
-    //           },
-    //           grid: {
-    //             left: '3%',
-    //             right: '4%',
-    //             bottom: '3%',
-    //             containLabel: true
-    //           },
-    //           xAxis: [
-    //             {
-    //               type: 'category',
-    //               data: echartsData[0],
-    //               axisTick: {
-    //                 alignWithLabel: true
-    //               }
-    //             }
-    //           ],
-    //           yAxis: [
-    //             {
-    //               type: 'value'
-    //             }
-    //           ],
-    //           series: [
-    //             {
-    //               name: '2024-4-27',
-    //               type: 'bar',
-    //               barWidth: '60%',
-    //               data: echartsData[1]
-    //             }
-    //           ]
-    //     }
-    //     // chart = echarts.init(chartRef.current);
-    //     // chart.setOption(option);
-    //     // return () => {
-    //     //     chart.dispose();
-    //     // };
-    // },[echartsData])
-    function handleFacChange(value: any) {
-        console.log(value);
-        setFactorLi(value)
-    }
-    function handleMoChange(value: any) {
-        setModeLi(value)
-    }
-    function handleQuery() {
-        postFetch()
+      setFactor(factorData);
+      setMode(modeData);
+    });
+  };
 
+  const postFetch = (option:any) => {
+    const data = {
+      factor: facotrLi,
+      mode: modeLi,
+      val: inputValue
+    };
+    request('quent-api/fetch', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: {
+        data: option
+      }
+    }).then(res => {
+      message.success(res.msg);
+      const resData = res.result;
+      const arr: any = [];
+      const title = Object.keys(resData);
+      const firstTitle = title[0];
+      const row = Object.keys(resData[firstTitle].TIME);
+      const columnsone = title.map(item => {
+        arr.push(Object.values(resData[item].result));
+        return {
+          title: item,
+          dataIndex: item,
+          key: item
+        };
+      });
+      const firstName = {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name'
+      };
+
+      const tableData = row.map((name, i) => {
+        const rowData = { name };
+        title.forEach((column, j) => {
+          rowData[column] = arr[j][i];
+        });
+        return rowData;
+      });
+      setColumns([firstName, ...columnsone]);
+      setDataSource(tableData);
+    });
+  };
+  useEffect(() => {
+    getFactor();
+  }, []);
+
+  function handleQuery(values: any) {
+    console.log();
+    if (values.data.length == 0) {
+      message.warning('请添加查询条件');
+    }else{
+      console.log(values);
+      postFetch(values.data)
     }
-    const handleInput = (e:any)=>{
-        setInputValue(e)
-    }
-    const handleAddElement = () =>{
-        setCondition((p:any)=>[...p,1])
-    }
-    return (
-        <div>  
-            {
-                condition.map((item,index)=>{
-                    return (
-                        <div style={{marginBottom:'5px'}} key={index} >
-                            <Select
-                                defaultValue="请选择条件"
-                                style={{ width: 240 }}
-                                onChange={handleFacChange}
-                                options={factor}
-                            />&nbsp;
-                            <Select
-                                defaultValue="请选择条件"
-                                style={{ width: 240 }}
-                                onChange={handleMoChange}
-                                options={mode}
-                            />&nbsp;
-                             <InputNumber 
-                                 defaultValue="0.01"
-                                 min="0"
-                                 max="10"
-                                 step="0.01"
-                                 stringMode
-                                onChange={handleInput}
-                            /> 
-                        </div>
-                    )
-                })
-            }
- 
-            <div style={{marginBottom:'5px'}}>
-            <Button onClick={handleAddElement}>添加条件</Button>&nbsp;&nbsp;
-            <Button type="primary" onClick={handleQuery}>查询</Button>
-            </div>
-          
-            <ProCard  bordered>
-                {/* <div ref={chartRef} style={{ width: "100%", height: "100%", overflow: 'hidden' }}></div> */}
-                <Table dataSource={dataSource} columns={columns} /> 
-            </ProCard>
-        </div>
-    )
-}
-export default Screen
+  }
+
+  return (
+    <div>
+      <Form name="筛选列表" onFinish={handleQuery} autoComplete="off">
+        <Form.List name="data">
+          {(fields, { add, remove }) => (
+            <>
+              {fields.map(({ key, name, ...restField }) => (
+                <Space
+                  key={key}
+                  style={{
+                    display: 'flex',
+                    marginBottom: -5
+                  }}
+                  align="baseline">
+                  <Form.Item
+                    {...restField}
+                    name={[name, 'factor']}
+                    rules={[
+                      {
+                        required: true,
+                        message: '请选择条件'
+                      }
+                    ]}>
+                    <Select
+                      defaultValue="请选择条件"
+                      style={{ width: 240 }}
+                      options={factor}
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    {...restField}
+                    name={[name, 'mode']}
+                    rules={[
+                      {
+                        required: true,
+                        message: '请选择条件'
+                      }
+                    ]}>
+                    <Select
+                      defaultValue="请选择条件"
+                      style={{ width: 240 }}
+                      options={mode}
+                    />
+                  </Form.Item>
+                  <Form.Item
+                    {...restField}
+                    name={[name, 'val']}
+                    rules={[
+                      {
+                        required: true,
+                        message: '请选择数值'
+                      }
+                    ]}>
+                    <InputNumber
+                      style={{
+                        width: 100
+                      }}
+                      min="0"
+                      max="10"
+                      step="0.01"
+                      stringMode
+                    />
+                  </Form.Item>
+                  <MinusCircleOutlined onClick={() => remove(name)} />
+                </Space>
+              ))}
+
+              <Form.Item>
+                <Space>
+                  <Button
+                    style={{ width: 130 }}
+                    type="dashed"
+                    onClick={() => add()}
+                    block
+                    icon={<PlusOutlined />}>
+                    新增条件
+                  </Button>
+                  <Button type="primary" htmlType="submit">
+                    查询
+                  </Button>
+                </Space>
+              </Form.Item>
+            </>
+          )}
+        </Form.List>
+      </Form>
+      <ProCard bordered>
+        <Table dataSource={dataSource} columns={columns} />
+      </ProCard>
+    </div>
+  );
+};
+export default Screen;
