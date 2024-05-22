59cf0099e2d6b6a3c9baa1b0c73399ea3a7e1c5d
王银鹏 <982172159@qq.com>
2024-05-22 16:18:48 +0800

    股票界面表格优化

--- src/pages/Home/components/News/Stocks/index.tsx
+++ src/pages/Home/components/News/Stocks/index.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { Table } from 'antd';
-import type { TableColumnsType, TableProps } from 'antd';
-import { getStock } from '../../../service';
-import styles from '../styles.less';
+import { getStock } from '@/pages/Home/service';
+import type { ProColumns } from '@ant-design/pro-components';
+import { ProTable } from '@ant-design/pro-components';
+import { useEffect, useState } from 'react';
+
 
 interface StockData {
   name: string;
@@ -10,48 +10,73 @@ interface StockData {
   [key: string]: string;
 }
 
-const Stocks: React.FC<TableProps<StockData>> = () => {
-  const [data, setData] = React.useState<any[]>([]);
+function createNewColumns(features: Record<string, any>) {
+  return Object.entries(features).map(([titleName, featureObj], index) => ({
+    title: titleName,
+    dataIndex: titleName,
+    key: titleName,
+    // width: ReactAutoFitColumns.getWidth(titleName),
+    width: 80,
+  }));
+}
 
-  const [columns, setColumns] = useState<TableColumnsType<StockData>>([
-    { title: '名称', dataIndex: 'name', width: 200, align: 'center' }
-  ]);
+function generateTargetData(features: Record<string, any>): StockData[] {
+  const stockNames = Object.values(features)[0].TIME;
+
+  return Object.entries(stockNames).map(([stockName]) => {
+    const stockData: StockData = {
+      name: stockName,
+      key: stockName,
+    };
+
+    Object.entries(features).forEach(([featureName, featureObj]) => {
+      const value = featureObj.result[stockName];
+      const roundedNumber = parseFloat(value.toFixed(3));
+      stockData[featureName] = roundedNumber;
+    });
+
+    return stockData;
+  });
+}
+
+const EnhancedRender = ({ record }: { record: StockData }) => (
+  <div>
+    <p>{record.name}的内容</p>
+  </div>
+);
+const extendedRender = (record: StockData) => <EnhancedRender record={record} />;
+
+export default () => {
+  const initialColumns: ProColumns<StockData>[] = [
+    {
+      title: '股票名称',
+      width: 60,
+      fixed: 'left',
+      dataIndex: 'name',
+      render: (_, record) => <a>{record.name}</a>,
+    },
+  ];
+
+  const [data, setData] = useState<any[]>([]);
+  const [mergedColumns, setMergedColumns] = useState<ProColumns<StockData>[]>(initialColumns);
   const [isLoading, setIsLoading] = useState(true);
-
   const fetchData = async () => {
-    const response = await getStock();
-    const { features = [], summary = [] } = response.result || {};
+    try {
+      setIsLoading(true);
+      const response = await getStock();
+      const { features = [], summary = [] } = response.result || {};
 
-    const newColumns: TableColumnsType<StockData> = Object.keys(features).map(titleName => ({
-      title: titleName,
-      dataIndex: titleName,
-      key: titleName,
-      width: 500,
-    }));
-    setColumns(prevColumns => [...prevColumns, ...newColumns]);
-    const targetData: StockData[] = [];
-    // 提取股票名称列表
-    const stockNames = Object.values(features)[0].TIME;
+      const newColumns = createNewColumns(features);
+      const combinedColumns = [...initialColumns, ...newColumns];
+      setMergedColumns(combinedColumns);
 
-    const featureEntries = Object.entries(features);
-
-    for (const stockName of Object.keys(stockNames)) {
-      const stockData = {
-        name: stockName,
-        key: stockName,
-      };
-
-      featureEntries.map(([featureName, featureObj]) => {
-        const value = featureObj.result[stockName];
-        const roundedNumber = parseFloat(value.toFixed(3));
-        stockData[featureName] = roundedNumber;
-      });
-
-      targetData.push(stockData);
+      const targetData = generateTargetData(features);
+      setData(targetData);
+    } catch (error) {
+      console.error('Fetch data error:', error);
+    } finally {
+      setIsLoading(false);
     }
-    console.log(targetData, '列表数据');
-    setData(targetData);
-    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -59,20 +84,23 @@ const Stocks: React.FC<TableProps<StockData>> = () => {
   }, []);
 
   return (
-    <div className={styles.content}>
-      <Table
-        columns={columns}
-        loading={isLoading}
-        rowKey={(record) => record.name}
-        expandable={{
-          expandedRowRender: (record) => (
-            <p style={{ margin: 0 }}>{record.name}的详细信息</p>
-          ),
-        }}
-        dataSource={data}
-      />
-    </div>
+    <ProTable
+      loading={isLoading}
+      columns={mergedColumns}
+      dataSource={data}
+      scroll={{ x: 2000 }}
+      options={false}
+      search={false}
+      pagination={{
+        pageSize: 10,
+      }}
+      expandable={{
+        columnWidth: 15,
+        expandedRowRender: (record) => (
+          extendedRender(record)
+        ),
+      }}
+      rowKey="key"
+    />
   );
 };
-
-export default Stocks;
