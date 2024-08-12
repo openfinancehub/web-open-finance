937a5aba029e6bc7d5e1c6e32eea6d94148a0d3f
王银鹏 <982172159@qq.com>
2024-08-12 16:24:34 +0800

    pdf

--- src/pages/Home/components/Info/Industry/Test.tsx
+++ src/pages/Home/components/Info/Industry/Test.tsx
@@ -2,54 +2,37 @@ import { Card } from 'antd';
 import React, { useState, useEffect, useRef } from 'react';
 // import viewer from '../pdfjs/web/viewer'
 // import * as pdfjsLib from 'pdfjs-dist/webpack';
-import * as pdf from 'pdfjs-dist'
-import pdfWorker from 'pdfjs-dist/build/pdf.worker.js?url'
+// import * as pdf from 'pdfjs-dist'
+// import pdfWorker from 'pdfjs-dist/build/pdf.worker.js?url'
+import RcResizeObserver from 'rc-resize-observer';
+import { ProCard, ProTable } from '@ant-design/pro-components';
 const Text: React.FC = () => {
     const fileInputRef = useRef<HTMLInputElement>(null);
-
-    const loadPdfFile = () => {
-        const fileInput = fileInputRef.current;
-        if (!fileInput) return;
-
-        const file = fileInput.files?.[0];
-        if (!file) return;
-
-        const blobUrl = URL.createObjectURL(file);
-        window.open(blobUrl, '_blank');
-    };
-
+    const [responsive, setResponsive] = useState(false);
     return (
-        <div>
-            <input type="file" accept=".pdf" ref={fileInputRef} />
-            <button onClick={loadPdfFile}>点击查看PDF</button>
-            {/* <viewer></viewer> */}
+        <RcResizeObserver
+            key="resize-observer"
+            onResize={(offset) => {
+                setResponsive(offset.width < 596);
+            }}
+        >
+            <ProCard split={responsive ? 'horizontal' : 'vertical'}>
+                <ProCard bordered >
+                    <a href="https://pdf.dfcfw.com/pdf/H2_AN202408061639155597_1.pdf?1722936327000.pdf" target="_blank">
+                        点击在新窗口查看
+                    </a>
+                </ProCard>
 
-            <Card title={'iframe方式'} style={{ width: '50%' }}>
-                <a href="https://pdf.dfcfw.com/pdf/H2_AN202408061639155597_1.pdf?1722936327000.pdf">
-                    点击查看
-                </a>
-            </Card>
+                <ProCard bordered  >
+                    <embed src="https://pdf.dfcfw.com/pdf/H2_AN202408061639155597_1.pdf?1722936327000.pdf#page=2"
+                        title="PDF Viewer"
+                        width="100%"
+                        height="500px">
+                    </embed>
+                </ProCard>
 
-            <Card title={'iframe方式'} style={{ width: '50%' }}>
-                <iframe src="https://pdf.dfcfw.com/pdf/H2_AN202408061639155597_1.pdf?1722936327000.pdf#page=2"
-                    title="PDF Viewer"
-                    width="100%"
-                    height="500px">
-                </iframe>
-            </Card>
-
-            <Card title={'embed方式'} style={{ width: '50%' }}>
-                <embed src="https://www.lilnong.top/static/pdf/B-4-RxJS%E5%9C%A8React%E4%B8%AD%E7%9A%84%E5%BA%94%E7%94%A8-%E9%BE%99%E9%80%B8%E6%A5%A0_.pdf#page=12"
-                    title="PDF Viewer"
-                    width="100%"
-                    height="500px">
-                </embed>
-
-                <embed src="https://pdf.dfcfw.com/pdf/H2_AN202408061639155597_1.pdf?1722936327000.pdf">
-                </embed>
-            </Card>
-
-        </div>
+            </ProCard>
+        </RcResizeObserver>
     );
 };
 
