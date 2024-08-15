431ff28d134934645e0e829bd1cac3bc75f3af70
舒航ya <2691958739@qq.com>
2024-08-15 17:36:17 +0800

    1

---
+++ src/pages/Analyze/components/Public/ToolDialog.jsx
@@ -0,0 +1,9 @@
+import { Box, Button, Typography, Dialog, DialogActions, DialogContent, DialogTitle, Stack, OutlinedInput } from '@mui/material'
+
+const ToolDialog = ({ show, dialogProps, onUseTemplate, onCancel, onConfirm, setError }) => {
+    return (
+        <Dialog>
+            
+        </Dialog>
+    )
+}
