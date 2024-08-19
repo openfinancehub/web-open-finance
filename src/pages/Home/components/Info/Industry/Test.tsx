790ba1f9ca515ce392ae0ad44ea5d333d188e17e
xw-wan <982172159@qq.com>
2024-08-19 15:11:51 +0800

    pdf

--- src/pages/Home/components/Info/Industry/Test.tsx
+++ src/pages/Home/components/Info/Industry/Test.tsx
@@ -1,29 +1,47 @@
-import { Card } from 'antd';
+import { Card, Drawer } from 'antd';
 import React, { useState, useEffect, useRef } from 'react';
-// import viewer from '../pdfjs/web/viewer'
-// import * as pdfjsLib from 'pdfjs-dist/webpack';
-import * as pdf from 'pdfjs-dist'
 import { ProCard, ProTable } from '@ant-design/pro-components';
+import { size } from 'lodash';
+
 const Text: React.FC = () => {
     const [responsive, setResponsive] = useState(false);
+    const [open, setOpen] = useState(false);
+
+    const showDrawer = () => {
+        setOpen(true);
+    };
+
+    const onClose = () => {
+        setOpen(false);
+    };
+
     return (
-
         <ProCard split={responsive ? 'horizontal' : 'vertical'}>
-
-            <ProCard bordered >
-                <a href="https://pdf.dfcfw.com/pdf/H2_AN202408061639155597_1.pdf?1722936327000.pdf" target="_blank">
-                    点击在新窗口查看
+            <ProCard bordered>
+                <a type="primary" onClick={showDrawer}>
+                    Open
                 </a>
+                <Drawer
+                    // title="Basic Drawer"
+                    placement="right"
+                    closable={false}
+                    size={'large'}
+                    onClose={onClose}
+                    open={open}
+                    getContainer={false}
+                    style={{ position: 'absolute', height: '80vh', }}
+                >
+                    <embed
+                        src="https://pdf.dfcfw.com/pdf/H2_AN202408191639329975_1.pdf?1724068313000.pdf#page=4"
+                        title="PDF Viewer"
+                        width="100%"
+                        height="500px"
+                    />
+                </Drawer>
             </ProCard>
-
-            <ProCard bordered  >
-                <embed src="https://pdf.dfcfw.com/pdf/H2_AN202408061639155597_1.pdf?1722936327000.pdf#page=2"
-                    title="PDF Viewer"
-                    width="100%"
-                    height="500px">
-                </embed>
-            </ProCard>
-        </ProCard>
+            {/* <ProCard bordered>
+            </ProCard> */}
+        </ProCard >
     );
 };
 
